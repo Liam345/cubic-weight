7e55a0c5d6e5f34044d8923685921cc348935822
Liam <liam205@outlook.com>
2017-12-23 04:40:07 +1100

    Added linting and Prettier

--- src/main.js
+++ src/main.js
@@ -1,37 +1,35 @@
 const filteredArr = [];
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const targetUrl = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com';
-const startEndpoint = '/api/products/1';
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const targetUrl = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com";
+const startEndpoint = "/api/products/1";
 const baseUrl = proxyUrl + targetUrl;
-const allowed = 'Air Conditioners';
+const allowed = "Air Conditioners";
 
 function filterByItem(raw) {
-  const filtered = raw.objects.filter(function(value) {
-    return value.category === allowed;
-  });
+  const filtered = raw.objects.filter(value => value.category === allowed);
   filteredArr.push(...filtered);
 }
 
-async function fetchProducts(baseUrl,endpoint) {
-    const response = await fetch(baseUrl+endpoint);
-    const data = await response.json();
-    filterByItem(data)
-    if(data.next != null){
-      fetchProducts(baseUrl,data.next);
-    }
-    else{
-      calculateCubicWeight();
-      return;
-    }
-    }
+const cWtReducer = (total, cur) =>
+  total + cur.size.width * cur.size.length * cur.size.height / 4000;
 
-function calculateCubicWeight(){
-  const totalCubicWeight = filteredArr.reduce(function(total,cur){
-           return total + (cur.size.width * cur.size.length * cur.size.height / 4000);
-  },0);
+function calculateCubicWeight() {
+  const totalCubicWeight = filteredArr.reduce(cWtReducer, 0);
   const totalItems = filteredArr.length;
   const avgCubicWeight = totalCubicWeight / totalItems;
   console.log(`Average cubic weight of ${allowed} is ${avgCubicWeight}`);
 }
 
-fetchProducts(baseUrl,startEndpoint);
+async function fetchProducts(endpoint) {
+  const response = await fetch(baseUrl + endpoint);
+  const data = await response.json();
+  filterByItem(data);
+  if (data.next != null) {
+    fetchProducts(data.next);
+  } else {
+    calculateCubicWeight();
+    // return;
+  }
+}
+
+fetchProducts(startEndpoint);
